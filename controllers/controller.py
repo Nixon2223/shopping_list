ba4f204ca0f3c8f07ffd88bd0e621847bec7ca35
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-04 15:06:08 +0000

    add item working

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,6 +2,11 @@ from flask import render_template, request, redirect
 from app import app
 from models.items import *
 
-@app.route('/list')
+@app.route('/shopping_list')
 def index():
     return render_template('index.html', title = 'Home', items = items)
+
+@app.route('/shopping_list', methods=['POST'])
+def add_item():
+  add_new_item(Item(request.form['name'], request.form['price'], request.form['quantity'], False ))
+  return redirect('/shopping_list')
--- models/items.py
+++ models/items.py
@@ -5,3 +5,6 @@ item2 = Item("bread", 1.00, 1, False)
 item3 = Item("bananas", 0.20, 4, False)
 
 items = [item1, item2, item3]
+
+def add_new_item(new_item):
+    items.append(new_item)
