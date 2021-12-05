06c180ed33c9ce5624832cef8683177d335eccac
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-05 17:47:31 +0000

    added totals, bought filter and remove

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,9 +4,30 @@ from models.items import *
 
 @app.route('/shopping_list')
 def index():
-    return render_template('index.html', title = 'Home', items = items)
+  t = total_price(items)
+  l = len(items)
+  return render_template('index.html', title = 'Home', items = items, t=t, l=l)
 
 @app.route('/shopping_list', methods=['POST'])
 def add_item():
   add_new_item(Item(request.form['name'], request.form['price'], request.form['quantity'], False ))
+  return redirect('/shopping_list')
+
+@app.route('/shopping_list/remove<int:i>')
+def order_remove(i):
+    items.pop(i)
+    return redirect('/shopping_list')
+
+@app.route('/shopping_list/bought<int:i>')
+def order_bought(i):
+    items[i].bought = not items[i].bought
+    return redirect('/shopping_list')
+
+@app.route('/shopping_list/bought_filter<string:i>')
+def order_bought_filter(i):
+  new_items = filter_bought(items, i)
+  t = total_price(new_items)
+  l = len(new_items)
+  if i == "T" or i == "F":
+    return render_template('index.html', title = 'Home', items = new_items, t=t, l=l)
   return redirect('/shopping_list')
--- models/items.py
+++ models/items.py
@@ -2,9 +2,38 @@ from models.item import *
 
 item1 = Item("tea bags", 1.50, 1, False)
 item2 = Item("bread", 1.00, 1, False)
-item3 = Item("bananas", 0.20, 4, False)
+item3 = Item("bananas", 0.20, 4, True)
+item4 = Item("tea bags", 1.50, 1, False)
+item5 = Item("bread", 1.00, 1, False)
+item6 = Item("bananas", 0.20, 4, False)
+item7 = Item("tea bags", 1.50, 1, False)
+item8 = Item("bread", 1.00, 1, True)
+item9 = Item("bananas", 0.20, 4, False)
 
-items = [item1, item2, item3]
+items = [item1, item2, item3, item4, item5, item6, item7, item8, item9]
 
 def add_new_item(new_item):
     items.append(new_item)
+
+def total_price(items):
+    total_price = 0
+    for item in items:
+          total_price += float(item.price) * float(item.quantity)
+    return "%.2f" % float(total_price)
+
+def filter_bought(items, i):
+    new_list = []
+    if i == "T":
+        for item in items:
+            if item.bought == True:
+                new_list.append(item)
+    if i == "A":
+        for item in items:
+                new_list.append(item)
+    if i == "F":
+        for item in items:
+            if item.bought == False:
+                new_list.append(item)
+    return new_list
+      
+
